0dff193035c5c93b9ec5b3b492a21cf077391a32
qycyfjy <kyaruny@gmail.com>
2023-11-12 20:04:10 +0800

    cha1 初始化nvvk::Context

--- _edit/main.cpp
+++ _edit/main.cpp
@@ -1,8 +1,14 @@
 // Copyright 2020 NVIDIA Corporation
 // SPDX-License-Identifier: Apache-2.0
-#include <iostream> 
+#include <iostream>
+#include <nvvk/context_vk.hpp>
 
-int main(int argc, const char** argv)
-{
-  std::cout << "Hello world!";
+int main(int argc, const char** argv) {
+    nvvk::ContextCreateInfo deviceInfo; // Context初始化的hints
+    nvvk::Context context; // instance, device, physical device, and queues
+    context.init(deviceInfo);
+
+    vkAllocateCommandBuffers(context, nullptr, nullptr); // 测试VK_LAYER_KHRONOS_validation
+
+    context.deinit();
 }
