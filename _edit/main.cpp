780f9e474efdfb49807f5dd075df7dfcf36c0568
qycyfjy <kyaruny@gmail.com>
2023-11-12 20:28:38 +0800

    cha2 添加ray query扩展

--- _edit/main.cpp
+++ _edit/main.cpp
@@ -1,14 +1,25 @@
 // Copyright 2020 NVIDIA Corporation
 // SPDX-License-Identifier: Apache-2.0
-#include <iostream>
+#include <cassert>
+
 #include <nvvk/context_vk.hpp>
+#include <nvvk/structs_vk.hpp>  // vulkan使用sType成员变量来识别void*指向什么结构, 用nvvk::make简化这部分的初始化
 
 int main(int argc, const char** argv) {
-    nvvk::ContextCreateInfo deviceInfo; // Context初始化的hints
-    nvvk::Context context; // instance, device, physical device, and queues
+    nvvk::ContextCreateInfo deviceInfo;
+    deviceInfo.apiMajor = 1;
+    deviceInfo.apiMinor = 3;
+    deviceInfo.addDeviceExtension(VK_KHR_DEFERRED_HOST_OPERATIONS_EXTENSION_NAME);
+    auto acceleration_structure_feature = nvvk::make<VkPhysicalDeviceAccelerationStructureFeaturesKHR>();
+    deviceInfo.addDeviceExtension(VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME, false, &acceleration_structure_feature);
+    auto ray_query_feature = nvvk::make<VkPhysicalDeviceRayQueryFeaturesKHR>();
+    deviceInfo.addDeviceExtension(VK_KHR_RAY_QUERY_EXTENSION_NAME, false, &ray_query_feature);
+
+    nvvk::Context context;
     context.init(deviceInfo);
 
-    vkAllocateCommandBuffers(context, nullptr, nullptr); // 测试VK_LAYER_KHRONOS_validation
+    assert(acceleration_structure_feature.accelerationStructure == VK_TRUE && ray_query_feature.rayQuery == VK_TRUE);
+
 
     context.deinit();
 }
