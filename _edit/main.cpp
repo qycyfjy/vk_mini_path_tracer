fae702cb5deb843e135fd586afe3b44550f4e0fa
qycyfjy <kyaruny@gmail.com>
2023-11-13 09:04:49 +0800

    cha4 commands 创建command pool -> 分配command buffer -> 开始记录command -> ... -> 结束记录 -> 提交command -> 等待queue -> 清理

--- _edit/main.cpp
+++ _edit/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 
 #include <nvvk/context_vk.hpp>
+#include <nvvk/error_vk.hpp> // NVVK_CHECK macro
 #include <nvvk/resourceallocator_vk.hpp>  // NVVK memeory allocators, vulkan低级到没提供malloc
 #include <nvvk/structs_vk.hpp>  // vulkan使用sType成员变量来识别void*指向什么结构, 用nvvk::make简化这部分的初始化
 
@@ -36,12 +37,58 @@ int main(int argc, const char** argv) {
                                          VK_MEMORY_PROPERTY_HOST_COHERENT_BIT | // CPU可缓存
                                          VK_MEMORY_PROPERTY_HOST_CACHED_BIT); // CPU自动缓存管理
 
+    auto cmd_pool_create_info = nvvk::make<VkCommandPoolCreateInfo>();
+    cmd_pool_create_info.queueFamilyIndex = context.m_queueGCT;
+    VkCommandPool cmd_pool;
+    // nullptr指使用默认的Vulkan CPU-side内存分配器
+    NVVK_CHECK(vkCreateCommandPool(context, &cmd_pool_create_info, nullptr, &cmd_pool));
+
+    auto cmd_buffer_allocate_info = nvvk::make<VkCommandBufferAllocateInfo>();
+    cmd_buffer_allocate_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    cmd_buffer_allocate_info.commandPool = cmd_pool;
+    cmd_buffer_allocate_info.commandBufferCount = 1;
+    VkCommandBuffer cmd_buffer;
+    NVVK_CHECK(vkAllocateCommandBuffers(context, &cmd_buffer_allocate_info, &cmd_buffer));
+
+    // begin record command
+    auto cmd_buffer_begin_info = nvvk::make<VkCommandBufferBeginInfo>();
+    cmd_buffer_begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT; // 一次录制只会使用command buffer一次
+    NVVK_CHECK(vkBeginCommandBuffer(cmd_buffer, &cmd_buffer_begin_info));
+
+    const float fill_value = 0.5f;
+    const uint32_t&fill_value_uint32 = reinterpret_cast<const uint32_t &>(fill_value);
+    vkCmdFillBuffer(cmd_buffer, buffer.buffer, 0, buffer_size_in_bytes, fill_value_uint32);
+
+    // 让CPU(host)读数据的操作等待transter(如vkCmdFillBuffer)完成
+    auto memory_barrier = nvvk::make<VkMemoryBarrier>();
+    memory_barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+    memory_barrier.dstAccessMask = VK_ACCESS_HOST_READ_BIT;
+    vkCmdPipelineBarrier(cmd_buffer,
+        VK_PIPELINE_STAGE_TRANSFER_BIT,
+        VK_PIPELINE_STAGE_HOST_BIT,
+
+        0,
+        1, &memory_barrier,
+        0, nullptr, 0, nullptr);
+
+    NVVK_CHECK(vkEndCommandBuffer(cmd_buffer));
+
+    auto submit_info = nvvk::make<VkSubmitInfo>();
+    submit_info.commandBufferCount = 1;
+    submit_info.pCommandBuffers = &cmd_buffer;
+    NVVK_CHECK(vkQueueSubmit(context.m_queueGCT, 1, &submit_info, VK_NULL_HANDLE));
+
+    // 让CPU等待GCT queue完成
+    NVVK_CHECK(vkQueueWaitIdle(context.m_queueGCT));
+
     // GPU -> CPU
     void* data = allocator.map(buffer);
     float* image = static_cast<float *>(data);
-    printf("first three: %f, %f, %f\n", image, image[1], image[2]);
+    printf("first three: %f, %f, %f\n", image[0], image[1], image[2]);
     allocator.unmap(buffer);
 
+    vkFreeCommandBuffers(context, cmd_pool, 1, &cmd_buffer);
+    vkDestroyCommandPool(context, cmd_pool, nullptr);
     allocator.destroy(buffer);
     allocator.deinit();
     context.deinit();
